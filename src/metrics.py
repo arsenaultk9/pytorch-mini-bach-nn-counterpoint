80a1fd3011bc002bbec35de924ad5a997ccc3420
arsenaultk9 <arsenaultk9@gmail.com>
2022-12-19 18:02:38 -0500

    Measure note accuracy metric implemented.

--- src/metrics.py
+++ src/metrics.py
@@ -45,11 +45,15 @@ def get_total_note_accuracy(model_output, y_target):
     for batch_index in range(constants.BATCH_SIZE):
         model_transposed = model_output[batch_index].transpose(0, 1)
 
-        for slice_index in range(constants.SEQUENCE_LENGTH):
-            model_prediction = torch.argmax(model_transposed[slice_index])
-            target_prediction = y_target[batch_index][slice_index]
+        for measure_index in range(constants.MEASURES):
+            measure_start = measure_index * constants.MEASURE_LENGTH
+            measure_end = (measure_index + 1) * constants.MEASURE_LENGTH
+            measure_target_notes = y_target[batch_index][measure_start:measure_end]
 
-            if model_prediction == target_prediction:
-                total_right_predictions += 1
+            for sequence_index in range(measure_start, measure_end):
+                model_prediction = torch.argmax(model_transposed[sequence_index])
+                
+                if (model_prediction.item() in measure_target_notes):
+                    total_right_predictions += 1
 
     return total_right_predictions
