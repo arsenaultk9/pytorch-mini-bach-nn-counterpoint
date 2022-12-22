536d34504948fdf505fb2c8a369b6e885370dabb
arsenaultk9 <arsenaultk9@gmail.com>
2022-12-22 08:28:26 -0500

    Plage in range for scale augmentation + toggle tensorboard runs.

--- src/models/voices.py
+++ src/models/voices.py
@@ -1,10 +1,18 @@
 from src.models.range import Range
 from src.models.voice import Voice
+import src.constants as constants
 
 voices = dict()
 
-# Ranges calculated from min/max of stats.py with SAT 20 range and B 27 range.
-voices['soprano'] = Voice('soprano', 0, Range(61, 81))
-voices['alto'] = Voice('alto', 1, Range(56, 76))
-voices['tenor'] = Voice('tenor', 2, Range(51, 71))
-voices['bass'] = Voice('bass', 3, Range(36, 63))
+if constants.APPLY_SCALE_AUGMENTATION:
+    # Calculated ranges + scale augmentation buffer
+    voices['soprano'] = Voice('soprano', 0, Range(61 - 5, 81 + 6))
+    voices['alto'] = Voice('alto', 1, Range(56 - 5, 76 + 6))
+    voices['tenor'] = Voice('tenor', 2, Range(51 - 5, 71 + 6))
+    voices['bass'] = Voice('bass', 3, Range(36 - 5, 63 + 6))
+else:
+    # Ranges calculated from min/max of stats.py with SAT 20 range and B 27 range.
+    voices['soprano'] = Voice('soprano', 0, Range(61, 81))
+    voices['alto'] = Voice('alto', 1, Range(56, 76))
+    voices['tenor'] = Voice('tenor', 2, Range(51, 71))
+    voices['bass'] = Voice('bass', 3, Range(36, 63))
